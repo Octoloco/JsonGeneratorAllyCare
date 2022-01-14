c8344bcae0eb1f5379df60a208d2061e81ed8d81
Diego Clorio <diego_clorio@hotmail.com>
2022-01-14 15:03:55 -0600

    Json basic generator
    Missing actual NFT content

---
+++ JsonGenerator/JsonGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace SerializeToFile
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string fileName = "C:/Users/Diego/Desktop/AllyCare/NFTs/WeatherForecast.json";
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            File.WriteAllText(fileName, jsonString);
+
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+    }
+}
