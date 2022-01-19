ed299576a7191db582b7b72a97edbd8ce92d5078
Diego Clorio <diego_clorio@hotmail.com>
2022-01-18 18:03:32 -0600

    NFT initial format implementation
    Missing correct attributes and IO

--- JsonGenerator/JsonGenerator/Program.cs
+++ JsonGenerator/JsonGenerator/Program.cs
@@ -4,29 +4,25 @@ using System.Text.Json;
 
 namespace SerializeToFile
 {
-    public class WeatherForecast
-    {
-        public DateTimeOffset Date { get; set; }
-        public int TemperatureCelsius { get; set; }
-        public string Summary { get; set; }
-    }
 
     public class Program
     {
+        private void Generate(int iterations, int startingIndex)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                string fileName = "C:/Users/Diego/Desktop/AllyCare/JsonGeneratorAllyCare/Jsons/" + i.ToString() + ".json";
+                string jsonString = "{\r\n\"name\": \"Test #" + i + "\",\r\n\"symbol\": \"NB\",\r\n\"description\": \"Collection of 22,000 numbers on the blockchain. This is the number " + i + "/22000.\",\r\n\"seller_fee_basis_points\": 500,\r\n\"image\": \"0.jpg\",\r\n\"attributes\": [\r\n{ \"trait_type\": \"Holidays\", \"value\": \"0\"},\r\n{ \"trait_type\": \"Courses\", \"value\": \"0\"},\r\n{ \"trait_type\": \"Yoga\", \"value\": \"1\"},\r\n{ \"trait_type\": \"Car\", \"value\": \"0\"},\r\n{ \"trait_type\": \"kit\", \"value\": \"1\"}\r\n],\r\n\"properties\": {\r\n\"creators\": [{ \"address\": \"8TXdPWnq8dB4Nnj1WxSqQnLJdbbduw9sQomsBZx7TCs3\", \"share\": 100}],\r\n\"files\": [{ \"uri\": \"0.jpg\", \"type\": \"image/jpg\"}]\r\n},\r\n\"collection\": { \"name\": \"ACTests\", \"family\": \"ACTests\"}\r\n}";
+                File.WriteAllText(fileName, jsonString);
+
+                Console.WriteLine(File.ReadAllText(fileName));
+            }
+        }
+
+
         public static void Main()
         {
-            var weatherForecast = new WeatherForecast
-            {
-                Date = DateTime.Parse("2019-08-01"),
-                TemperatureCelsius = 25,
-                Summary = "Hot"
-            };
 
-            string fileName = "C:/Users/Diego/Desktop/AllyCare/NFTs/WeatherForecast.json";
-            string jsonString = JsonSerializer.Serialize(weatherForecast);
-            File.WriteAllText(fileName, jsonString);
-
-            Console.WriteLine(File.ReadAllText(fileName));
         }
     }
 }
